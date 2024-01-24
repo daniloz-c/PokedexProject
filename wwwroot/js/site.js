5f2463e54547579998b12316948072f381f821e3
Danilo Cesar <109434051+daniloz-c@users.noreply.github.com>
2024-01-23 21:12:33 -0300

    adding files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,50 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+
+const fetchPokemon = async (pokemon) => {
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (APIresponse.status === 200) {
+        const data = await APIresponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    const data = await fetchPokemon(pokemon);
+    if (data) {
+        pokemonImage.style.display = 'block'
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none'
+        pokemonName.innerHTML = 'Not Found!'
+        pokemonNumber.innerHTML = '';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+    input.value = '';
+});
+
+
+renderPokemon(searchPokemon);
